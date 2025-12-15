c92ffa3907d542268c7acd2f258b227f5bb7e8cb
mikkiDev <mikkimail.games@gmail.com>
2025-12-15 16:08:46 +0100

    "fixed" cmakelists.txt

---
+++ src/main.cpp
@@ -0,0 +1,25 @@
+#ifndef NODES
+#define NODES 100
+#endif
+
+#ifndef TICKS
+#define TICKS 500000
+#endif
+
+#ifndef PGEN
+#define PGEN 0.02
+#endif
+
+#ifndef CW
+#define CW 16
+#endif
+
+
+#include <iostream>
+#include <cstdlib>
+
+
+int main(int argc, char* argv[]) {
+    std::cout << "ao" << std::endl;
+    return 0;
+}
